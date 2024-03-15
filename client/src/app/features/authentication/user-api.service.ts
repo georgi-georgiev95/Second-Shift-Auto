b82d35e6568a7ae34376e9d7b9b7efff6b233458
Georgi957 <gesh007@abv.bg>
2024-03-15 14:06:49 +0200

    Rename service

--- client/src/app/features/authentication/user-api.service.ts
+++ client/src/app/features/authentication/user-api.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class UserApiService {
 
   constructor(private http: HttpClient) { }
 
