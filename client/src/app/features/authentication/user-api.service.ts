def5733d1bc1ec09b125b220feac022b71dc07b1
Georgi957 <gesh007@abv.bg>
2024-03-15 20:54:47 +0200

    Create subject to store user info

--- client/src/app/features/authentication/user-api.service.ts
+++ client/src/app/features/authentication/user-api.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { UserLogin, UserReg } from 'src/app/types/user.interface';
+import { AuthRes, UserLogin, UserReg } from 'src/app/types/user.interface';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
+  private user$$ = new BehaviorSubject<AuthRes>({} as AuthRes); 
+  user$ = this.user$$.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+      
+   }
 
   login(formData: UserLogin) {
-    return this.http.post<UserLogin>('/users/login', formData);
+    return this.http
+      .post<AuthRes>('/users/login', formData)
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 
   register(formData: UserReg) {
-    return this.http.post<UserReg>('/users/register', formData);
+    return this.http
+      .post<AuthRes>('/users/register', formData)
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 }
