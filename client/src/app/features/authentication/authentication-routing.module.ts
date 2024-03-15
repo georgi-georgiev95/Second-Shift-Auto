f30527f6d4f826c63578cc5ab66c9d55624b1541
Georgi957 <gesh007@abv.bg>
2024-03-15 13:35:34 +0200

    Include login component in routes

--- client/src/app/features/authentication/authentication-routing.module.ts
+++ client/src/app/features/authentication/authentication-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
         path: 'register',
         component: RegisterComponent,
       },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
     ],
   },
 ];
