4fd1811567d25e5b87e3e7696dd102e4b3485299
Georgi957 <gesh007@abv.bg>
2024-03-15 12:40:11 +0200

    Create reactive form initial structure

--- client/src/app/features/authentication/register/register.component.ts
+++ client/src/app/features/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = this.fb.group({
+    username: '',
+    email: '',
+    passGroup: this.fb.group({
+      password: '',
+      rePassword: ''
+    })
+  });
 
+  constructor(private fb: FormBuilder){}
 }
