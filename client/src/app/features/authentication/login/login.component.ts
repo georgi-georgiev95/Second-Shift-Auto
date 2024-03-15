988ae46a7c9bab41cad57cb1062e284dc020cabc
Georgi957 <gesh007@abv.bg>
2024-03-15 15:46:28 +0200

    Import user api service in component

--- client/src/app/features/authentication/login/login.component.ts
+++ client/src/app/features/authentication/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserApiService } from '../user-api.service';
+import { UserLogin } from 'src/app/types/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +15,16 @@ export class LoginComponent {
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private userApiService: UserApiService, private router: Router) {}
 
   loginUser(): void {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
     }
+    
+    const formData = this.loginForm.value;
+    this.userApiService.login(formData as UserLogin).subscribe(() => {
+      this.router.navigate(['/home']);
+    });
   }
 }
