f0e97381ad99d6b2445f059466292bcce77d1836
Georgi957 <gesh007@abv.bg>
2024-03-15 13:34:37 +0200

    Create login component

--- client/src/app/features/authentication/authentication.module.ts
+++ client/src/app/features/authentication/authentication.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
