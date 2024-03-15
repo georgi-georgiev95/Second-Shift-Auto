9cc702634c78838871f8dfa46c5742657634200d
Georgi957 <gesh007@abv.bg>
2024-03-15 13:59:13 +0200

    Import HttpClient

--- client/src/app/features/authentication/authentication.service.ts
+++ client/src/app/features/authentication/authentication.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
 }
