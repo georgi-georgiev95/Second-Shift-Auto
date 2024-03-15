78936560b8289cc51854b0dcfbe402fd1bae1427
Georgi957 <gesh007@abv.bg>
2024-03-15 16:18:14 +0200

    Update server's cors policy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ const { auth } = require("./middlewares/authMiddleware");
 const app = express();
 
 // !Avoid CORS error
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true
+}));
 app.use(express.json());
 app.use(auth);
 app.use(routes);
