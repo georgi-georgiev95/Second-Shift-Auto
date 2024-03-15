a1a282c5c623aa0f5c3b2cb6e721f490f5ba5f48
Georgi957 <gesh007@abv.bg>
2024-03-15 13:48:27 +0200

    Add validations to login form

--- client/src/app/features/authentication/login/login.component.ts
+++ client/src/app/features/authentication/login/login.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   loginForm = this.fb.group({
-    email: '',
-    password: ''
-  })
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor(private fb: FormBuilder) { }
-  
+  constructor(private fb: FormBuilder) {}
+
   loginUser(): void {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
