894de807e1a28e877ce5dcb7ee537192b6ba4f00
Georgi957 <gesh007@abv.bg>
2024-03-17 13:39:51 +0200

    Create Sell component

--- client/src/app/features/car/car.module.ts
+++ client/src/app/features/car/car.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CarRoutingModule } from './car-routing.module';
+import { AddCarComponent } from './add-car/add-car.component';
 
 
 
 @NgModule({
   declarations: [
-    CatalogComponent
+    CatalogComponent,
+    AddCarComponent
   ],
   imports: [
     CommonModule,
