3cab8eb8720069949b11aecc1fd127a6c7afd7f6
Georgi957 <gesh007@abv.bg>
2024-03-17 13:29:05 +0200

    Get all cars from server

--- client/src/app/features/car/car-api.service.ts
+++ client/src/app/features/car/car-api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { Car } from 'src/app/types/car.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getCars() {
+    return this.http
+      .get<Car[]>('/cars/catalog');
+  }
 }
