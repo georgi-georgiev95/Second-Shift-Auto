fbcbbf7b8a77261e1b8381d45767d12a468cb8cc
Georgi957 <gesh007@abv.bg>
2024-03-17 11:49:54 +0200

    Implement route guard for authentication

--- client/src/app/features/authentication/authentication-routing.module.ts
+++ client/src/app/features/authentication/authentication-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthActivate } from 'src/app/core/guards/auth.guard';
 
 const routes: Routes = [
       {
         path: 'register',
         component: RegisterComponent,
+        canActivate: [AuthActivate],
       },
       {
         path: 'login',
-        component: LoginComponent
+        component: LoginComponent,
+        canActivate: [AuthActivate],
       }
 ];
 
