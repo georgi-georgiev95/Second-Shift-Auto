71844b8b0c9e1847ca42c848b72a0a67ff50ef95
Georgi957 <gesh007@abv.bg>
2024-03-17 12:07:43 +0200

    Extract user data from local storage

--- client/src/app/features/authentication/profile/profile.component.ts
+++ client/src/app/features/authentication/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Profile } from 'src/app/types/user.interface';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  userData: Profile = JSON.parse(localStorage.getItem('userData') || '{}');
 
 }
