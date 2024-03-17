0f71b6b5d1e07815a203fd1373e49a31a2a7308d
Georgi957 <gesh007@abv.bg>
2024-03-17 13:40:00 +0200

    Include sell to router

--- client/src/app/features/car/car-routing.module.ts
+++ client/src/app/features/car/car-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
+import { AddCarComponent } from './add-car/add-car.component';
 
 const routes: Routes = [
   {
     path: 'catalog',
     component: CatalogComponent,
   },
+  {
+    path: 'sell',
+    component: AddCarComponent
+  }
 ];
 
 @NgModule({
