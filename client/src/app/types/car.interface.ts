d2dac7036187bdc781cda603004ccb1e0767e498
Georgi957 <gesh007@abv.bg>
2024-03-17 12:28:05 +0200

    Create Car interface

---
+++ client/src/app/types/car.interface.ts
@@ -0,0 +1,42 @@
+enum EngineType {
+  Petrol,
+  Diesel,
+  Electric,
+  Hybrid,
+}
+
+enum GearBox {
+  Manual,
+  Automatic,
+}
+
+enum Category {
+  Hatchback,
+  Sedan,
+  SUV,
+  Wagon,
+  Coupe,
+}
+
+interface additionalImages {
+  url: string;
+}
+
+export interface Car {
+  _id?: string;
+  make: string;
+  model: string;
+  price: number;
+  year: number;
+  engineType: EngineType;
+  power: number;
+  gearbox: GearBox;
+  category: Category;
+  mileage: number;
+  color: string;
+  description: string;
+  image: string;
+  location: string;
+  additionalImages: additionalImages[];
+  error?: string;
+}
