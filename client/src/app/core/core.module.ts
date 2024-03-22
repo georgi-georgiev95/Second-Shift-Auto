25c8a335068d047d25b3fc62da079f77fb75b72a
Georgi957 <gesh007@abv.bg>
2024-03-22 20:14:02 +0200

    Exported the wrapping component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterLink } from '@angular/router';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    AuthenticateComponent
   ],
   imports: [
     CommonModule,
     RouterLink
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    AuthenticateComponent
   ]
 })
 export class CoreModule { }
