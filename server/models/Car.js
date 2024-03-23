e6c8ad86bb04bb25a608d88736269f7f8140f6b9
Georgi957 <gesh007@abv.bg>
2024-03-23 15:25:03 +0200

    Added buyer to Car model

--- server/models/Car.js
+++ server/models/Car.js
@@ -15,7 +15,8 @@ const carSchema = new mongoose.Schema({
     image: { type: String, required: true },
     location: { type: String, required: true },
     additionalImages: { type: Array },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User"}
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    buyer: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
 
 const Car = mongoose.model("Car", carSchema);
