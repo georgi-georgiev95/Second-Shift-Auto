7d1392848e8eb3d75740b296b9f8936cd1ec126e
Georgi957 <gesh007@abv.bg>
2024-03-30 11:14:39 +0200

    Update shared.module.ts

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { CurrencyFormaterPipe } from './pipes/currency-formater.pipe';
+import { ErrorComponent } from './error/error.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    CurrencyFormaterPipe
+    CurrencyFormaterPipe,
+    ErrorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
   ],
   exports: [
     LoaderComponent,
-    CurrencyFormaterPipe
+    CurrencyFormaterPipe,
+    ErrorComponent
   ]
 })
 export class SharedModule { }
